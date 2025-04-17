fe1912d3d665628d544ec6b1d20fba9da19bf597
Y-Akinci <yaren-can@hotmail.ch>
2025-04-17 16:48:06 +0200

    Update guest_dal.py

--- data_access/guest_dal.py
+++ data_access/guest_dal.py
@@ -1,1 +1,14 @@
+from data_access.base_dal import BaseDal
+from model.guest import Guest
+from model.booking import Booking
 
+class GuestDal(BaseDal):
+  def create_guest(self, guest_id:int, first_name:int, last_name:int, email:str):
+    sql = """
+      INSERT INTO Guest (guest_id, first_name, last_name, email)
+      VALUES(?, ?, ?, ?)
+    """
+    params = (guest_id, first_name, last_name, email)
+    last_id, _ = self.execute(sql, params)
+    return Guest(last_id, guest_id, first_name, last_name, email)
+    
