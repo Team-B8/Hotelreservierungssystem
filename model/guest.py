fd10c01c9aec7c00a373844c56cde1487b4a96e5
Y-Akinci <yaren-can@hotmail.ch>
2025-04-18 23:54:59 +0200

    Update guest.py

--- model/guest.py
+++ model/guest.py
@@ -5,8 +5,8 @@ class Guest:
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
-        self._bookings = []
-        self._is_deleted = False
+        self._bookings = []  # list to store associated bookings
+        self._is_deleted = False  # flag to mark logical deletion
 
     @property
     def guest_id(self):
@@ -26,6 +26,7 @@ class Guest:
 
     @first_name.setter
     def first_name(self, new_first_name):
+        # update first name only if it is different
         if new_first_name != self._first_name:
             self._first_name = new_first_name
         else:
@@ -33,6 +34,7 @@ class Guest:
 
     @last_name.setter
     def last_name(self, new_last_name):
+        # update last name only if it is different
         if new_last_name != self._last_name:
             self._last_name = new_last_name
         else:
@@ -40,12 +42,14 @@ class Guest:
 
     @email.setter
     def email(self, new_email):
+        # update email only if it is different
         if self.email != new_email:
             self._email = new_email
         else:
-            print(f"The new email ist the same as before.")
+            print(f"The new email is the same as before.")
 
     def delete(self):
+        # marks the guest as logically deleted
         if not self._is_deleted:
             self._is_deleted = True
             print(f"{self._first_name} {self._last_name} was marked as deleted.")
@@ -57,6 +61,7 @@ class Guest:
         return self._is_deleted
 
     def restore(self):
+        # reverses the logical deletion of the guest
         if self._is_deleted:
             self._is_deleted = False
             print(f"{self._first_name} {self._last_name} was restored.")
@@ -72,6 +77,7 @@ class Guest:
         return self._bookings
 
     def create_booking(self, room, check_in, check_out):
+        # creates a new booking and adds it to the list of this guest
         new_booking = Booking(room, check_in, check_out)
         self._bookings.append(new_booking)
         return new_booking
