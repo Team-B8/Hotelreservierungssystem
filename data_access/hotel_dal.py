9dfc339cb7322eaca50b4013d9b5312fa0bcc11d
Haris-1-1 <harissalii745@gmail.com>
2025-04-18 23:21:16 +0200

    Create hotel_dal.py

---
+++ data_access/hotel_dal.py
@@ -0,0 +1,27 @@
+from data_access.base_dal import BaseDal
+import model.hotel as model
+
+class HotelDAL(BaseDal):
+    def create_hotel(self, name: str, stars: int) -> model.Hotel:
+        sql = """
+        INSERT INTO Hotel (Name, Stars) VALUES (?, ?)
+        """
+        params = (name, stars)
+        last_row_id, _ = self.execute(sql, params)
+        return model.Hotel(last_row_id, name, stars)
+
+    def read_hotel_by_id(self, hotel_id: int) -> model.Hotel | None:
+        sql = """
+        SELECT HotelId, Name, Stars FROM Hotel WHERE HotelId = ?
+        """
+        params = (hotel_id,)
+        result = self.fetchone(sql, params)
+        if result:
+            hotel_id, name, stars = result
+            return model.Hotel(hotel_id, name, stars)
+        return None
+
+    def delete_hotel(self, hotel_id: int) -> None:
+        sql = "DELETE FROM Hotel WHERE HotelId = ?"
+        params = (hotel_id,)
+        self.execute(sql, params)
