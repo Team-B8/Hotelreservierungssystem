2da237b9330ccdcffb289662ded7e743f581c0d3
Haris-1-1 <harissalii745@gmail.com>
2025-04-18 23:22:10 +0200

    Update hotel_manager.py

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -1,1 +1,19 @@
+import model.hotel as model
+import data_access.hotel_dal as data_access
+
+class HotelManager:
+    def __init__(self) -> None:
+        self.__hotel_dal = data_access.HotelDAL()
+
+    def create_hotel(self, name: str, stars: int) -> model.Hotel:
+        # Business-Logik: Stars müssen zwischen 1 und 5 sein
+        if not (1 <= stars <= 5):
+            raise ValueError("Stars must be between 1 and 5")
+        return self.__hotel_dal.create_hotel(name, stars)
+
+    def get_hotel(self, hotel_id: int) -> model.Hotel | None:
+        return self.__hotel_dal.read_hotel_by_id(hotel_id)
+
+    def delete_hotel(self, hotel_id: int) -> None:
+        self.__hotel_dal.delete_hotel(hotel_id)
 
