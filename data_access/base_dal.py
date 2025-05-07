e33225861910b28cca4c14ad0656fb8ff9ee0c53
lounico84 <lou.brauchli@gmail.com>
2025-05-07 11:49:35 +0200

    change

--- data_access/base_dal.py
+++ data_access/base_dal.py
@@ -1,4 +1,22 @@
+from datetime import date, datetime
 import sqlite3
+
+import shutil
+source = "database/media_store_o.db"
+db_file = "database/working_db.db"
+shutil.copyfile(source, db_file) #Copy original DB first to always start fresh.
+
+def date_to_db(d: date) -> str:
+    return d.isoformat()
+
+def db_to_date(s: str) -> date:
+    return datetime.strptime(s.decode(), "%Y-%m-%d").date()
+
+## Adapter: Wandelt `date`-Objekt in `TEXT` um
+sqlite3.register_adapter(date, date_to_db)
+
+## Konverter: Wandelt gespeicherte `TEXT`-Werte wieder in `date`
+sqlite3.register_converter("DATE", db_to_date)
 
 class BaseDal:
     def __init__(self, connection_str: str = None):
