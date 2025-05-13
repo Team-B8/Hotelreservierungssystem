bb07148287a0c0f2a8c66f724e13c3556717bed3
Haris-1-1 <harissalii745@gmail.com>
2025-05-13 22:06:09 +0200

    Update hotel_dal.py

--- data_access/hotel_dal.py
+++ data_access/hotel_dal.py
@@ -3,17 +3,17 @@ from model.hotel import Hotel
 
 class HotelDAL(BaseDal):
     def create_hotel(self, name: str, stars: int) -> model.Hotel:
-        sql = """
+        sql = "
         INSERT INTO Hotel (Name, Stars) VALUES (?, ?)
-        """
+        "
         params = (name, stars)
         last_row_id, _ = self.execute(sql, params)
         return model.Hotel(last_row_id, name, stars)
 
     def read_hotel_by_id(self, hotel_id: int) -> model.Hotel | None:
-        sql = """
+        sql = "
         SELECT HotelId, Name, Stars FROM Hotel WHERE HotelId = ?
-        """
+        "
         params = (hotel_id,)
         result = self.fetchone(sql, params)
         if result:
