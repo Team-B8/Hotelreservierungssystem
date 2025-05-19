612289d2211f478fd313d4e53c368301a9c8479e
lounico84 <lou.brauchli@gmail.com>
2025-05-19 18:23:15 +0200

    added gast_menu for the guest user too choose a user story

--- ui_folder/input_helper.py
+++ ui_folder/input_helper.py
@@ -58,3 +58,31 @@ def user_story_1_6():
     for hotel in hotels:
         address = hotel.get_address()
         print(f"{hotel.get_name()} | {hotel.get_stars()} Sterne | Adresse: {address.get_full_address()}")
+
+def gast_menu():
+    while True:
+        print("\n--- GAST MENÜ ---")
+        print("1.1 Hotels nach Stadt filtern")
+        print("1.2 Hotels nach Sternen in Stadt filtern")
+        print("1.3 Hotels mit passenden Zimmern für Gästezahl")
+        print("1.4 Hotels mit verfügbaren Zimmern im Zeitraum")
+        print("1.5 Hotels nach Stadt, Sterne, Gästezahl, Zeitraum")
+        print("1.6 Hotelinformationen anzeigen")
+        print("0. Zurück zum Hauptmenü")
+        auswahl = input("Option wählen: ")
+        if auswahl == "1.1":
+            user_story_1_1()
+        elif auswahl == "1.2":
+            user_story_1_2()
+        elif auswahl == "1.3":
+            user_story_1_3()
+        elif auswahl == "1.4":
+            user_story_1_4()
+        elif auswahl == "1.5":
+            user_story_1_5()
+        elif auswahl == "1.6":
+            user_story_1_6()
+        elif auswahl == "0":
+            break
+        else:
+            print("Ungültige Eingabe.")
