2b600828d86198c0edaf4cb61ef0f7b863725999
lounico84 <lou.brauchli@gmail.com>
2025-05-19 18:19:02 +0200

    First Import and date function in input file

--- ui_folder/input_helper.py
+++ ui_folder/input_helper.py
@@ -1,1 +1,9 @@
+from business_logic.hotel_manager import HotelManager
+from datetime import datetime
 
+def input_date(prompt):
+    while True:
+        try:
+            return datetime.strptime(input(prompt + ' (YYYY-MM-DD): '), "%Y-%m-%d").date()
+        except ValueError:
+            print("Ungültiges Datum. Bitte erneut eingeben.")
