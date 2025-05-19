6e78cb65d241e8ed0bd059d095109ec004788e1f
lounico84 <lou.brauchli@gmail.com>
2025-05-19 18:20:15 +0200

    Created first User Story in input file

--- ui_folder/input_helper.py
+++ ui_folder/input_helper.py
@@ -7,3 +7,10 @@ def input_date(prompt):
             return datetime.strptime(input(prompt + ' (YYYY-MM-DD): '), "%Y-%m-%d").date()
         except ValueError:
             print("Ungültiges Datum. Bitte erneut eingeben.")
+
+def user_story_1_1():
+    print("\n--- 1.1: Hotels nach Stadt filtern ---")
+    city = input("Stadt eingeben: ")
+    hotels = HotelManager().filter_by_city(city)
+    for hotel in hotels:
+        print(f"ID: {hotel.get_hotel_id()} | Name: {hotel.get_name()} | Sterne: {hotel.get_stars()}")
