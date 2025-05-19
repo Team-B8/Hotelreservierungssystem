2097d9facab132e03cdda75170bd7f03276cb1c2
lounico84 <lou.brauchli@gmail.com>
2025-05-19 18:36:29 +0200

    updated model hotel

--- model/hotel.py
+++ model/hotel.py
@@ -1,31 +1,49 @@
 class Hotel:
-    def __init__(self, hotel_id: int, name: str, stars: int):
-        self.__hotel_id = hotel_id
-        self.__name = name
-        self.__stars = stars
+    def __init__(self, hotel_id: int, name: str, stars: int, address_id: int):
+        self.hotel_id = hotel_id
+        self.name = name
+        self.stars = stars
+        self.address_id = address_id
 
-    def get_hotel_id(self) -> int:
+    def __repr__(self):
+        return f"Hotel(id={self.__hotel_id}, name='{self.__name}', stars={self.__stars}, address_id={self.__address_id})"
+
+    @property
+    def hotel_id(self) -> int:
         return self.__hotel_id
 
-    def get_name(self) -> str:
+    @hotel_id.setter
+    def hotel_id(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Hotel-ID muss ein Integer sein.")
+        self.__hotel_id = value
+
+    @property
+    def name(self) -> str:
         return self.__name
 
-    def set_name(self, name: str) -> None:
-        self.__name = name
+    @name.setter
+    def name(self, value: str):
+        if not value or not isinstance(value, str):
+            raise ValueError("Name muss ein nicht-leerer String sein.")
+        self.__name = value
 
-    def get_stars(self) -> int:
+    @property
+    def stars(self) -> int:
         return self.__stars
 
-    def set_stars(self, stars: int) -> None:
-        self.__stars = stars
+    @stars.setter
+    def stars(self, value: int):
+        if not isinstance(value, int) or not (1 <= value <= 5):
+            raise ValueError("Sterne müssen ein Integer zwischen 1 und 5 sein.")
+        self.__stars = value
 
-    def delete(self) -> None:
-        del self
+    @property
+    def address_id(self) -> int:
+        return self.__address_id
 
-    def get_rooms(self) -> list:
-        # Placeholder for getting rooms associated with the hotel
-        return []
-
-    def get_available_rooms(self, start: str, end: str) -> list:
-        # Placeholder for checking available rooms in the given date range
-        return []
+    @address_id.setter
+    def address_id(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Address-ID muss ein Integer sein.")
+        self.__address_id = value
