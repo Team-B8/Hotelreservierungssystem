d9043744e2a7bb34e3442449bf7e66e1f5bf91b5
Lou Brauchli <lou.brauchli@gmail.com>
2025-05-22 16:02:52 +0200

    chaned room_type_manager.py to business logic class, was a model class

--- business_logic/room_type_manager.py
+++ business_logic/room_type_manager.py
@@ -1,41 +1,14 @@
-
+from data_access.room_type_dal import RoomTypeDAL
+from model.room_type import RoomType
 
 class RoomTypeManager:
+    def __init__(self):
+        self.dal = RoomTypeDAL()
 
-    def __init__(self, room_type_id: int, description: str, max_guests: int):
+    def get_by_id(self, type_id: int) -> RoomType | None:
+        # Returns a single RoomType by its ID
+        return self.dal.get_by_id(type_id)
 
-        if not room_type_id:
-            raise ValueError("Room type id required")
-        if not isinstance(room_type_id, int):
-            raise ValueError("Room type id must be an integer")
-        if not description:
-            raise ValueError("A descrition is required")
-        if not isinstance(description, str):
-            raise ValueError("The description must be a string")
-        if not max_guests:
-            raise ValueError("Maximum guest capacity required")
-        if not isinstance(max_guests, int):
-            raise ValueError("Maximum guest capacity must be an integer")
-
-    self.__room_type_id: int = room_type_id
-    self.__description: str = description
-    self.__max_guests: int = max_guests
-
-    def __repr__(self):
-        return f"Room type id: {room_type_id}\nRoom type description: {description}\nMaximum guest capacity: {max_guest}"
-
-
-#Getter
-    @property
-    def room_type_id(self):
-        return self.__room_type_id
-
-    @property
-    def description(self):
-        return self.__description
-
-    @property
-    def max_guests(self):
-        return self.__max_guests
-
-#Setter
+    def get_all(self) -> list[RoomType]:
+        # Returns a list of all RoomTypes
+        return self.dal.get_all()
