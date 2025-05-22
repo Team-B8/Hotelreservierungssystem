42727bf229ceb7fadbc940220beb0cef88b9b147
Lou Brauchli <lou.brauchli@gmail.com>
2025-05-22 15:52:01 +0200

    changed room_manager class to business logic class, was a model class

--- business_logic/room_manager.py
+++ business_logic/room_manager.py
@@ -1,88 +1,11 @@
-# 
-
-from business_logic.facilities_manager import Facilities
+from data_access.room_dal import RoomDAL
+from model.room import Room
 
 class RoomManager:
-    """
-    Model Class Room
-    """
 
-    def __init__(self, room_id: int, room_no: int, price_per_night: float):
+    def __init__(self):
+        self.room_dal = RoomDAL()
 
-# Input Validation -> reacts to certain types of possible false inputs with a helping response
-        if not room_id:
-            raise ValueError("room_id is required")
-        if not isinstance(room_id, int):
-            raise ValueError("room_id must be an integer")
-        if not room_no:
-            raise ValueError("room_no is required")
-        if not isinstance(room_no, int):
-            raise ValueError("room_no must be an integer")
-        if not price_per_night:
-            raise ValueError("price_per_night is required")
-
-# Attribute initialization: ID, No, Price
-
-        self.__room_id: int = room_id
-        self.__room_no: int = room_no
-        self.__price_per_night: float = price_per_night
-        self.__facility: int = []
-
-    def __repr__(self): #Format in which whole object is represented -> __repr__
-        return (f"Room Id = {self.__room_id}, Room No. = {self.__room_no}, Price per night: {self.__price_per_night}")
-
-#Getter for each variable with use of decorators
-    @property
-    def room_id(self) -> int:
-        return self.__room_id
-
-    @property
-    def room_no(self) -> int:
-        return self.__room_no
-    
-    @property
-    def price_per_night(self) -> float:
-        return self.__price_per_night
-
-#Setter for each variable with use of decorators
-    @room_id.setter
-    def room_id(self, room_id: int) -> None:
-        if not room_id:
-            raise ValueError("ID is required")
-        if not isinstance(room_id, int):
-            raise ValueError("ID must me an integer")
-        self.__room_id = room_id
-
-    @room_no.setter
-    def room_no(self, room_no: int) -> None:
-        if not room_no:
-            raise ValueError("No. is required")
-        if not isinstance(room_no, int):
-            raise ValueError("No. must me an integer")
-        self.__room_no = room_no
-
-    @price_per_night.setter
-    def price_per_night(self, price_per_night: int) -> None:
-        if not price_per_night:
-            raise ValueError("Price per night is required")
-        if not isinstance(price_per_night, float):
-            raise ValueError("Price per night must be a float")
-        self.__price_per_night = price_per_night
-
-#Adding a facility
-    def add_facility(self, facility):
-        if facility not in self.__facility:
-            self.__facility.append(facility)
-        facilities.room = self
-
-
-## Mit Hotel verbunden. Hotel soll Room hinzufügen können als auch entfernen.
-
-
-
-
-
-
-
-
-
+    def get_rooms_by_hotel_id(self, hotel_id: int) -> list[Room]:
+        # return all rooms for a given hotel
+        return self.room_dal.get_rooms_by_hotel_id(hotel_id)
