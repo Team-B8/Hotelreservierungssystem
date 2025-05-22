bff798455e2d295dce0c05b49082522c671c8328
Lou Brauchli <lou.brauchli@gmail.com>
2025-05-22 16:00:44 +0200

    changed facilities_manager.py class, was a model class

--- business_logic/facilities_manager.py
+++ business_logic/facilities_manager.py
@@ -16,9 +16,11 @@ class FacilitiesManager:
         return self.dal.create(new_facility)
 
     def get_facility_by_id(self, facility_id: int) -> Facilities | None:
+        # Get a facility by its ID
         return self.dal.get_by_id(facility_id)
 
     def delete_facility(self, facility_id: int) -> bool:
+        # Delete a facility by its ID
         return self.dal.delete(facility_id)
 
     def assign_facility_to_room(self, facility: Facilities, room):
@@ -26,57 +28,6 @@ class FacilitiesManager:
         facility.assign_to_room(room)
         room.add_facility(facility)
 
-from business_logic.room import Room
-
-class Facilities:
-    """
-    Model Class Facilities
-    """
-    def __init__(self, facility_id: int, facility: str):
-
-#Input Validation
-        if not facility_id:
-            raise ValueError("ID is required")
-        if not isinstance(facility_id, int):
-            raise ValueError("ID must be integer")
-        if not facility:
-            raise ValueError("Facility is required")
-        if not isinstance(facility, str):
-            raise ValueError("Facility must be a string")
-    
-#Initialization: Facility ID, Name
-    self.__facility_id = facility_id
-    self.__facility: str = facility
-    self.__room_no: Rooms int = [] #How should the relation be set???
-
-#Formatted representation of class
-    def __repr__(self):
-        return f"Facility(id={self.__facility_id}, name={self.__name})"
-
-#Getter
-    @property
-    def facility_id(self) -> int:
-        return self.__facility_id
-
-
-    @property 
-    def name(self) -> int:
-        return self.__facility
-
-### Braucht es so viele setter wann macht es sinn?
-#Setter
-    @facility_id.setter
-    def facility_id(self, facility_id):
-        if not facility_id:
-            raise ValueError("ID is required")
-        if not isinstance(facility_id, int):
-            raise ValueError("ID must be integer")
-        self.__facility_id = facility_id
-
-    @facility.setter
-    def facility(self, facility) -> None:
-        if not name:
-            raise ValueError("Name is required")
-        if not isinstance(facility, str):
-            raise ValueError("Name must be a string")
-        self.__facility = facility
+    def get_facilities_by_room_id(self, room_id: int) -> list[Facilities]:
+        # Get all facilities assigned to a specific room
+        return self.dal.get_facilities_by_room_id(room_id)
