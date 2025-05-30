f0f81cfaa880f5965feafbc9f4c1a428d24755d6
Lou Brauchli <lou.brauchli@gmail.com>
2025-05-30 16:09:39 +0200

    added functions

--- business_logic/room_type_manager.py
+++ business_logic/room_type_manager.py
@@ -12,3 +12,15 @@ class RoomTypeManager:
     def get_all(self) -> list[RoomType]:
         # Returns a list of all RoomTypes
         return self.dal.get_all()
+    
+    def create_room_type(self, description: str, max_guests: int) -> RoomType:
+        # create a new room type using the data access layer
+        return self.dal.create(description, max_guests)
+
+    def update_room_type(self, type_id: int, new_description: str, new_max_guests: int) -> bool:
+        # update an existing room type by ID
+        return self.dal.update(type_id, new_description, new_max_guests)
+
+    def delete_room_type(self, type_id: int) -> bool:
+        # delete a room type by ID
+        return self.dal.delete(type_id)
