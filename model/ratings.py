c7a6be501b2ce637a6b93c17f9e6143af41e12e7
Y-Akinci <yaren-can@hotmail.ch>
2025-06-06 22:10:18 +0200

    Rating: Date Datentyp angepasst, Val und Type Errors,, hotel und guest IDs, __repr__ hinzugefügt und Setter angepasst. Delete gelöscht

--- model/ratings.py
+++ model/ratings.py
@@ -1,9 +1,35 @@
+from datetime import date
+
 class Rating:
-    def __init__(self, rating_id: int, stars: int, comment: str, created_date: str):
+    def __init__(self, rating_id: int, stars: int, comment: None, created_date: date, hotel_id: int, guest_id: int):
+        if not rating_id:
+            raise ValueError("rating_id is required")
+        if not stars:
+            raise ValueError("stars is required")
+        if not hotel_id:
+            raise ValueError("hotel_id is required")
+        if not guest_id:
+            raise ValueError("guest_id is required")
+        if not isinstance(rating_id, int):
+            raise TypeError("rating_id must be an integer")
+        if not isinstance(guest_id, int):
+            raise TypeError("guest_id must be an integer")
+        if not isinstance(hotel_id, int):
+            raise TypeError("hotel_id must be an integer")
+        if not isinstance(stars, int) or not (1 <= stars <= 5):
+            raise ValueError("Stars must be an Integer between 1 an 5.")
+        
         self.__rating_id = rating_id
         self.__stars = stars
         self.__comment = comment
         self.__created_date = created_date
+        self.__hotel_id = hotel_id
+        self.__guest_id = guest_id
+    
+
+
+    def __repr__(self):
+        return f"Rating(id={self.__rating_id}, stars={self.__stars}, comment{self.__comment}, created_date{self.__created_date}, hotel_id{self.__hotel_id}. guest_id{self.__guest_id})"
 
     @property
     def rating_id(self) -> int:
@@ -13,18 +39,22 @@ class Rating:
     def stars(self) -> int:
         return self.__stars
 
-    def set_stars(self, stars: int) -> None:
-        self.__stars = stars
+    @stars.setter
+    def stars(self, value: int):
+        if not isinstance(value, int) or not (1 <= value <= 5):
+            raise ValueError("Stars musst be an Integer between 1 an 5.")
+        self.__stars = value
+
     @property
     def comment(self) -> str:
         return self.__comment
 
-    def set_comment(self, comment: str) -> None:
-        self.__comment = comment
+    @comment.setter
+    def comment(self, value: str):
+        if not isinstance(value, str) and value is not None:
+            raise TypeError("comment must be a string or None")
+        self.__comment = value
 
     @property
-    def created_date(self) -> str:
-        return self.__created_date
-
-    def delete(self) -> None:
-        del self
+    def created_date(self) -> date:
+        return self.__created_date    
