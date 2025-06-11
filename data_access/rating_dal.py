d35720f11f498f0a14842ce90ca19c82fed633fd
Y-Akinci <yaren-can@hotmail.ch>
2025-06-11 13:41:08 +0200

    löschen überflüssigen Methodenm, die nicht für jew. User Story zu brauchen sind

--- data_access/rating_dal.py
+++ data_access/rating_dal.py
@@ -21,21 +21,3 @@ class RatingDAL(BaseDAL):
             cursor = conn.execute(sql, (hotel_id,))
             rows = cursor.fetchall()
         return [Rating(*row) for row in rows]
-    
-    #def read_rating_by_id(self, rating_id: int) -> Rating | None:
-        sql = "SELECT RatingId, Stars, Comment, CreatedDate FROM Rating WHERE RatingId = ?"
-        params = (rating_id,)
-        with self._connect() as conn:
-            cursor = conn.execute(sql, params)
-            result = cursor.fetchone()
-        if result:
-            rating_id, stars, comment, created_date = result
-            return Rating(rating_id, stars, comment, created_date)
-        return None
-
-    #def delete_rating(self, rating_id: int) -> None:
-        sql = "DELETE FROM Rating WHERE RatingId = ?"
-        params = (rating_id,)
-        with self._connect() as conn:
-            conn.execute(sql, params)
-            conn.commit()
